125108d56724257b4008a0d995df6ab3b7a99fe2
Vicente Martínez Franco <vicentemartinezf@gmail.com>
2018-08-30 10:51:55 +0200

    Animaciones para menu 1

--- Recursos/Javascript/menu.js
+++ Recursos/Javascript/menu.js
@@ -3,11 +3,21 @@
 */
 
 function extenderMenu() {
-	document.getElementById('menu').style.visibility='visible';
+	document.getElementById('menu').style.height='100%';
 	document.getElementById('botMenu').setAttribute('onclick','ocultarMenu()');
 }
 
 function ocultarMenu() {
-	document.getElementById('menu').style.visibility='hidden';
+	document.getElementById('menu').style.height='0';
 	document.getElementById('botMenu').setAttribute('onclick','extenderMenu()');
+}
+
+function extenderMenu2() {
+	document.getElementById('menu2').style.height='50%';
+	document.getElementById('botMenu2').setAttribute('onclick','ocultarMenu2()');
+}
+
+function ocultarMenu2() {
+	document.getElementById('menu2').style.height='0';
+	document.getElementById('botMenu2').setAttribute('onclick','extenderMenu2()');
 }
